28ec73dc0d6de79efc8b67e83a56d7ac1eb42a74
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-08-26 21:14:41 -0700

    Add picture

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import Sharon from '../assets/Sharon.jpeg';
 
 function AboutMe(props) {
-    return <h2>TLC Cleaning Team</h2>
+    return (
+        <div className='Sharon'>
+            <h2>TLC Cleaning Team</h2>
+            <img src={Sharon} alt="Sharon" />
+        </div>
+    );
 }
 
 export default AboutMe;
